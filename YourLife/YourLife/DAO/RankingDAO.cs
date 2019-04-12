6321a60e71b7602ac9415c8552d5d9319621713e
FELIPE MELCHIOR DE BRITTO <u18200@academico.cotuca.unicamp.br>
2019-04-12 10:53:06 -0300

    criei o aplicativo asp MVC e fiz várias alterações

---
+++ YourLife/YourLife/DAO/RankingDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using YourLife.Models;
+
+namespace YourLife.DAO
+{
+    public class RankingDAO
+    {
+        public void Adiciona(Ranking rk)
+        {
+            using (var context = new RankingContext())
+            {
+                context.Ranking.Add(rk);
+                context.SaveChanges();
+            }
+        }
+        public IList<Ranking> Lista()
+        {
+            using (var contexto = new RankingContext())
+            {
+                return contexto.Ranking.ToList();
+            }
+        }
+    }
+}
