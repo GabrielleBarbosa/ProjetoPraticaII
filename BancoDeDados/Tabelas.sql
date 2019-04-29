3ec0c00d051ce1fe21b93cbab1ccddfc2009ca29
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-04-29 11:58:40 -0300

    Revert "Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/GabrielleBarbosa/ProjetoPraticaII"""""
    This reverts commit 8f3e0b370f65f1f6229e3bda1ecd8a4435345630.

--- BancoDeDados/Tabelas.sql
+++ BancoDeDados/Tabelas.sql
@@ -15,15 +15,6 @@ create table Jogador
 alter table Jogador alter column nickName varchar(25)
 drop table Jogador
 
-
-
-insert into Jogador values('Vinschers',0,'M',0,0,0,0,0,'S',1)
-insert into Jogador values('Gabs123',0,'F',0,0,0,0,0,'S',1)
-insert into Jogador values('Janies',0,'F',0,0,0,0,0,'S',1)
-
-------------------------------------------------------------------------------------------------------------------------
-
-
 create table Ranking
 (
 nickName varchar(25),
@@ -31,18 +22,7 @@ constraint fkJogador foreign key(nickName) references Jogador(nickName),
 pontos int
 )
 
-
 alter table Ranking alter column nickName no check constraint
-
-
-insert into Ranking values('Vinschers',120111)
-insert into Ranking values('Gabs123', 9929)
-insert into Ranking values('Janies', 543322)
-
-alter table Ranking add id int identity(1,1) primary key 
-
-drop table Ranking
-------------------------------------------------------------------------------------------------------------------------
 
 create table Escolha
 (
@@ -55,8 +35,7 @@ consequencia2 int,
 constraint fkConsequencia2 foreign key(consequencia2) references Consequencia(codConsequencia)
 
 )
-drop table Escolha
-------------------------------------------------------------------------------------------------------------------------
+
 create table Consequencia
 (
 codConsequencia int primary key,
@@ -66,8 +45,7 @@ tipoDoPontoGanho varchar(20),
 tipoDoPontoPerdido varchar(20),
 pontosPerdidos int
 )
-drop table Consequencia
-------------------------------------------------------------------------------------------------------------------------
+
 create table Emprego
 (
 codEmprego int primary key,
@@ -75,25 +53,6 @@ pontosNecessarios int,
 trabalho varchar(30),
 salario money
 )
-drop table Emprego
-alter table Emprego alter column trabalho varchar(100)
-select * from Emprego
-
-insert into Emprego values (1, 60,'Atendente de Mercado',1143.50)
-insert into Emprego values (2, 200,'Vendedor de Loja',1870.89 )
-insert into Emprego values (3, 30,'Lixeiro',998.50)
-insert into Emprego values (4, 200,'Pedreiro',1000)
-insert into Emprego values (5, 400,'Policial',4500.47)
-insert into Emprego values (6, 750,'Programador Iniciante',3345.43)
-insert into Emprego values (7, 250,'Garçom',2200.56)
-insert into Emprego values (8, 600,'Engenheiro',5000)
-insert into Emprego values (9, 500,'Professor',3896.78)
-insert into Emprego values (10, 900,'Piloto',8235.90)
-insert into Emprego values (11, 860,'Gestor de Qualidade',11235.90)
-
-
-
-------------------------------------------------------------------------------------------------------------------------
 
 create table AcontecimentoFixo
 (
@@ -103,8 +62,6 @@ codEscolha int,
 constraint fkEscolha foreign key(codEscolha) references Escolha(codEscolha)
 
 )
-drop table AcontecimentoFixo
-------------------------------------------------------------------------------------------------------------------------
 
 create table AcontecimentoAleatorio
 (
@@ -113,32 +70,17 @@ acontecimento ntext,
 codEscolha int,
 constraint fkEscolha2 foreign key(codEscolha) references Escolha(codEscolha)
 )
-drop table AcontecimentoAleatorio
-------------------------------------------------------------------------------------------------------------------------
 
 create table Mercado
 (
 id int primary key, 
-tipoProduto varchar (30),
-descricao ntext,
-preco money,
-produto varchar(100)
+produto varchar(200),
+descricao varchar(200),
+preco float
 )
 
 drop table Mercado
-select * from Mercado
-alter table Mercado alter column descricao ntext
-alter table Mercado add produto varchar(100) 
-alter table Mercado alter column preco money
-alter table Mercado alter column tipoProduto varchar(30)
 
-insert into Mercado values (1, 'Moradia', 'Casa pequena, 1 banheiro, 1 quarto, 1 sala, 1 cozinha, 1 Lavanderia', 50000,'Casa')
-insert into Mercado values (2, 'Automovel', 'Carro para 4 pessoas, popular', 46690,'Chelovret Jonix')
-insert into Mercado values (3, 'Moradia', 'Apartamento médio, 2 quartos, 2 banheiros, 1 Cozinha, 1 Lavanderia,1 sala', 100000,'Apartamento')
-insert into Mercado values (4, 'Automovel', 'Carro para 4 pessoas, popular', 54450,'Zolksvagem Golo ')
-insert into Mercado values (5, 'Moradia', 'Casa grande, 4 banheiros, 3 quartos, 1 sala, 1 escritório, 1 cozinha, 1 Lavanderia, Quintal com jardim, estacionamento para 2 carros', 1100000,'Casa')
-insert into Mercado values (4, 'Automovel', 'Carro para 4 pessoas, esportivo', 84490,'Revault Sombrero ')
-------------------------------------------------------------------------------------------------------------------------
 
 
 create table MercadoJogador
@@ -149,9 +91,22 @@ constraint fkJogador2 foreign key(codJogador) references Jogador(id)
 drop table MercadoJogador
 
 select * from MercadoJogador
-------------------------------------------------------------------------------------------------------------------------
+
+insert into Jogador values('Vinschers',0,'M',0,0,0,0,0,'S',1)
+insert into Jogador values('Gabs123',0,'F',0,0,0,0,0,'S',1)
+insert into Jogador values('Janies',0,'F',0,0,0,0,0,'S',1)
 
 
+insert into Ranking values('Vinschers',120111)
+insert into Ranking values('Gabs123', 9929)
+insert into Ranking values('Janies', 543322)
+
+select * from Ranking
 
 
+alter table Ranking add id int identity(1,1) primary key 
 
+
+insert into Mercado values (124, 'Casa', 'minha casa', 50000)
+insert into Mercado values (111, 'Ap', 'apzao', 120000)
+insert into Mercado values (222, 'Cachiorroi', 'dog', 12)
