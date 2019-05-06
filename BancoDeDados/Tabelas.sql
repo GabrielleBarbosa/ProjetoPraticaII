eeeaa83197d2eb8b4d7c37f9b9b706b2193668f1
FELIPE MELCHIOR DE BRITTO <u18200@academico.cotuca.unicamp.br>
2019-05-06 11:45:27 -0300

    Update Tabelas.sql

--- BancoDeDados/Tabelas.sql
+++ BancoDeDados/Tabelas.sql
@@ -78,17 +78,27 @@ drop table Emprego
 alter table Emprego alter column trabalho varchar(100)
 select * from Emprego
 
-insert into Emprego values (1, 60,'Atendente de Mercado',1143.50)
-insert into Emprego values (2, 200,'Vendedor de Loja',1870.89 )
-insert into Emprego values (3, 30,'Lixeiro',998.50)
-insert into Emprego values (4, 200,'Pedreiro',1000)
-insert into Emprego values (5, 400,'Policial',4500.47)
-insert into Emprego values (6, 750,'Programador Iniciante',3345.43)
-insert into Emprego values (7, 250,'Garçom',2200.56)
-insert into Emprego values (8, 600,'Engenheiro',5000)
-insert into Emprego values (9, 500,'Professor',3896.78)
-insert into Emprego values (10, 900,'Piloto',8235.90)
-insert into Emprego values (11, 860,'Gestor de Qualidade',11235.90)
+--essa coluna restrição é pra definir o que a pessoa precisa ter de formação acadêmica para ter acesso a esse emprego. Exemplo:
+-- Se a pessoa for formada como um professor, ela vai possuir acesso a todos os empregos relacionados a essa área, no caso '111', o código do emprego
+
+
+alter table Emprego alter column restricao int
+--quantos anos trabalhando que o jogador precisa ter pra ter acesso a esse emprego?
+alter table Emprego add anosExperiencia int
+
+insert into Emprego values (1, 60,'Atendente de Mercado',1143.50,888,0)
+insert into Emprego values (2, 200,'Vendedor de Loja',1870.89,888,0 )
+insert into Emprego values (3, 30,'Lixeiro',998.50,888,0)
+insert into Emprego values (4, 200,'Pedreiro',1000,888,0)
+insert into Emprego values (5, 400,'Policial',4500.47,0)
+insert into Emprego values (6, 750,'Programador Iniciante',3345.43,222,0)
+insert into Emprego values (7, 250,'Garçom',2200.56,888,0)
+insert into Emprego values (8, 600,'Engenheiro',5000,555,0)
+insert into Emprego values (9, 500,'Professor',3896.78,111,0)
+insert into Emprego values (10, 900,'Piloto',8235.90,999,0)
+insert into Emprego values (11, 860,'Gestor de Qualidade',11235.90,222,20)
+insert into Emprego values (12, 700,'Advogado',10000.67,666,0)
+insert into Emprego values (13, 750,'Juiz', 30.000,666,30000,30)
 
 
 
