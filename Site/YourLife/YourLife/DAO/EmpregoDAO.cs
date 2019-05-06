be954767b6869d13d871e4477a3488edd17f5990
FELIPE MELCHIOR DE BRITTO <u18200@academico.cotuca.unicamp.br>
2019-05-06 11:46:09 -0300

    Create EmpregoDAO.cs

---
+++ Site/YourLife/YourLife/DAO/EmpregoDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YourLife.Models;
+
+namespace YourLife.DAO
+{
+    public class EmpregoDAO
+    {
+
+        public void Adiciona(Emprego emp)
+        {
+            using (var context = new JogoContext())
+            {
+                context.Emprego.Add(emp);
+                context.SaveChanges();
+            }
+        }
+        public IList<Emprego> ListarEmprego()
+        {
+            using (var contexto = new JogoContext())
+            {
+                return contexto.Emprego.ToList();
+            }
+        }
+    }
+}
