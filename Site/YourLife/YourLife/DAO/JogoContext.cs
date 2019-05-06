63ea68e1ac0392e820deb57bf21159994b90b49a
FELIPE MELCHIOR DE BRITTO <u18200@academico.cotuca.unicamp.br>
2019-05-06 11:46:51 -0300

    update Emprego

--- Site/YourLife/YourLife/DAO/JogoContext.cs
+++ Site/YourLife/YourLife/DAO/JogoContext.cs
@@ -12,6 +12,7 @@ namespace YourLife.DAO
         public DbSet<Jogador> Jogador { get; set; }
         public DbSet<Ranking> Ranking { get; set; }
         public DbSet<Mercado> Mercado { get; set; }
+        public DbSet<Emprego> Emprego { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=regulus;Initial Catalog=PR118200;User ID=PR118200;Password=PR118200");
