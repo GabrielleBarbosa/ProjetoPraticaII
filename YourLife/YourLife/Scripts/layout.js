9d49bad1f44f1104a811a2f7b23dd7df8ebfb9c3
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-05-08 09:30:12 -0300

    apaguei uns bagui q não funcionava

--- YourLife/YourLife/Scripts/layout.js
+++
@@ -1,8 +0,0 @@
-function abrir(link)
-{
-    window.open(link);
-}
-
-function mudarBackground() {
-    $("#Jogo").css("background-image", "url('C:\Users\u18183\Documents\GitHub\ProjetoPraticaII\YourLife\YourLife\html\Imagens\sala_de_escola.jpg')");
-}
