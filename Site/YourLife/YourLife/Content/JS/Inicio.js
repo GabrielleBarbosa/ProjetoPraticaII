741dc9f2d8f42c7733e38392f75975714554400c
FelipeMDB <42344147+FelipeMDB@users.noreply.github.com>
2019-05-15 22:01:52 -0300

    jgjyg

--- Site/YourLife/YourLife/Content/JS/Inicio.js
+++ Site/YourLife/YourLife/Content/JS/Inicio.js
@@ -1,3 +1,21 @@
 ﻿$(document).ready(function () {
     $('.modal').modal();
 });
+
+///////////////////////////////////////////////////////////////////
+var modal = document.getElementById("modal1");
+var span = document.getElementsByClassName("close")[0];
+
+abrirModal = function () {
+    modal.style.display = "block";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
