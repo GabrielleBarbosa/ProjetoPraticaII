a0a7cf7c9535392be1b87d32506915391274bb57
felipemelchior112 <felipemelchior112@gmail.com>
2019-05-16 09:57:55 -0300

    glub glub

--- Site/YourLife/YourLife/Content/JS/Inicio.js
+++ Site/YourLife/YourLife/Content/JS/Inicio.js
@@ -1,17 +1,4 @@
-﻿///////////////////////////////////////////////////////////////////
-var modal = document.getElementById("modal1");
-var span = document.getElementsByClassName("close")[0];
-
-abrirModal = function () {
-    modal.style.display = "block";
-};
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
-span.onclick = function () {
-    modal.style.display = "none";
-};
+﻿
+$(document).ready(function () {
+    $('.modal').modal();
+});
