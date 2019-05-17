e811b76e2c17e039cd441fd2ccbaacd0d8b6425c
felipemelchior112 <felipemelchior112@gmail.com>
2019-05-17 11:52:24 -0300

    a

---
+++ Site/YourLife/YourLife/Content/JS/Outros.js
@@ -0,0 +1,17 @@
+﻿var texto = "";
+
+function suicidioTexto() {
+$('#txt1').hide();
+$('.lbl1').html("");
+$('#cabecalho').html("Tem certeza que deseja cometer suicídio?");
+}
+
+function terminarRel() {
+    $('#txt1').hide();
+    $('.lbl1').html("");
+    $('#cabecalho').html("Tem certeza que deseja terminar o relacionamento?");
+}
+
+$(document).ready(function () {
+    $('.modal').modal();
+});
