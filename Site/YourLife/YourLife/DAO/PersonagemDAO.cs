806462136f77a6f4aadef2c1533e6f9efe0c0501
felipemelchior112 <felipemelchior112@gmail.com>
2019-05-24 11:10:21 -0300

    Update PersonagemDAO.cs

--- Site/YourLife/YourLife/DAO/PersonagemDAO.cs
+++ Site/YourLife/YourLife/DAO/PersonagemDAO.cs
@@ -61,9 +61,9 @@ namespace YourLife.DAO
         {
             using (var contexto =  new JogoContext())
             {
-                IList<CursoJogador> cj = (IList<CursoJogador>)contexto.CursoJogador.Select(CursoJogador => p.Id);
-                IList<MercadoJogador> mj = (IList<MercadoJogador>)contexto.MercadoJogador.Select(MercadoJogador => p.Id);
-                foreach(CursoJogador cj1 in cj)
+                IList<CursoJogador> cj = contexto.CursoJogador.Where(c => c.codJogador == p.Id).ToList();
+                IList<MercadoJogador> mj = contexto.MercadoJogador.Where(m => m.CodJogador == p.Id).ToList();
+                foreach (CursoJogador cj1 in cj)
                 contexto.CursoJogador.Remove(cj1);
                 foreach(MercadoJogador mj1 in mj)
                 contexto.MercadoJogador.Remove(mj1);
