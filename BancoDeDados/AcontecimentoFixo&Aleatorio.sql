1d63d9c568f9da651dcf47d05c4779247ea4f063
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-05-27 12:04:49 -0300

    mudando as tabelas de escolha e acontecimentos

--- BancoDeDados/AcontecimentoFixo&Aleatorio.sql
+++ BancoDeDados/AcontecimentoFixo&Aleatorio.sql
@@ -8,6 +8,10 @@ constraint fkEscolha foreign key(codEscolha) references Escolha(id)
 select * from AcontecimentoFixo
 drop table AcontecimentoFixo
 
+insert into AcontecimentoFixo values (1, 'Você atingiu 16 anos! Está na hora de decidir se irá tirar sua carteira de motorista, é preciso muita responsabilidade e te custará $1500', 2)
+insert into AcontecimentoFixo values (2, 'Você atingiu 16 anos! Está na hora de decidir se irá tirar sua carteira de motorista, é preciso muita responsabilidade e te custará $1500', 4)
+insert into AcontecimentoFixo values (3, 'Você atingiu 18 anos! Agora você pode consumir bebidas alcoólicas, mas também ser preso, viva a maioridade!', 3) 
+
 
 ------------------------------------------------------------------------------------------------------------------------
 
@@ -24,3 +28,12 @@ drop table AcontecimentoAleatorio
 -- 1 a 15
 
 insert into AcontecimentoAleatorio values (1, 'Você tem uma festa no final de semana e uma prova segunda... Irá à festa ou estudará para a prova?', 1)
+insert into AcontecimentoAleatorio values (2, 'Jogar vídeo game ou estudar para a prova no dia seguinte?', 11)
+insert into AcontecimentoAleatorio values (3, 'Seu amiguinho te empurrou', 10)
+
+insert into AcontecimentoAleatorio values (21, 'Seu melhor amigo tem sentimentos por você e te pediu em namoro', 5)
+insert into AcontecimentoAleatorio values (22, 'Sua melhor amiga tem sentimentos por você e te pediu em namoro', 6)
+insert into AcontecimentoAleatorio values (23, 'Seu namorado(a) terminou com você =(', 7)
+insert into AcontecimentoAleatorio values (24, 'Seu namorado(a) terminou com você =(', 8)
+insert into AcontecimentoAleatorio values (25, 'Você tem uma festa de aniversário e está muito doente, mas ainda assim deseja muito ir... O que fazer?', 9)
+insert into AcontecimentoAleatorio values (25, '', 11)
--- BancoDeDados/Escolha&Consequencia.sql
+++ BancoDeDados/Escolha&Consequencia.sql
@@ -8,22 +8,64 @@ constraint fkConsequencia1 foreign key(consequencia1) references Consequencia(id
 consequencia2 int,
 constraint fkConsequencia2 foreign key(consequencia2) references Consequencia(id)
 )
+
 drop table Escolha
 
+insert into Escolha values(2, 'Tirar a carta', 'Não tirar a carta', 3, 17)
+insert into Escolha values(4, 'Tirar a carta', 'Não tirar a carta', 4, 17)
+insert into Escolha values(3, 'Sair para uma festa e comemorar', 'Procurar um plano de carreira', 5, 6)
+
+
 insert into Escolha values(1, 'Ir à festa', 'Estudar', 1, 2)
+insert into Escolha values(11, 'Jogar video game', 'Estudar', 7, 8)
+insert into Escolha values(10, 'Empurrar de volta', 'Ignorar e continuar brincando', 17, 18)
+
+insert into Escolha values(5, 'Aceitar', 'Negar e manter a amizade', 9, 10)
+insert into Escolha values(6, 'Aceitar', 'Negar e manter a amizade', 11, 10)
+insert into Escolha values(7, 'Insistir para continuarem juntos', 'Aceitar sua escolha', 13, 14)
+insert into Escolha values(8, 'Insistir para continuarem juntos', 'Aceitar sua escolha', 14, 14)
+insert into Escolha values(9, 'Ir à festa mesmo assim', 'Ficar em casa descansando', 15, 16)
 ------------------------------------------------------------------------------------------------------------------------
 
 create table Consequencia
 (
 id int primary key,
-cenario varchar(20),
+assunto varchar(20),
 pontosGanhos int,
-tipoDoPontoGanho varchar(20),
-tipoDoPontoPerdido varchar(20),
-pontosPerdidos int
+tipoDoPontoGanho char,
+tipoDoPontoPerdido char,
+pontosPerdidos int,
+resultado ntext
 )
+alter table consequencia add resultado ntext
 drop table Consequencia
 
-insert into Consequencia values(1, 'festa', 50, 'Felicidade','Inteligencia', 50)
-insert into Consequencia values(2, 'escola', 50, 'Inteligencia', 'Felicidade', 50)
+insert into Consequencia values(1, 'pontos', 50, 'F','I', 50, 'Você foi à festa, se divertiu muito, mas foi mal na prova, como esperado.')
+insert into Consequencia values(2, 'pontos', 50, 'I', 'F', 50, 'A prova foi um sucesso, infelizmente agora todos os seus amigos não param de falar da festa em que você não foi.')
+
+insert into Consequencia values(3, 'carteira', 1, '', '', 0, 'Parabéns! Agora você pode dirigir!')
+insert into Consequencia values(4, 'carteira', 0, '', '', 0, 'Quem sabe na próxima... Não saber dirigir não é o fim do mundo!')
+insert into Consequencia values(17, 'carteira', 0, '', '', 0, 'Não saber dirigir não é o fim do mundo, não é mesmo?')
+
+insert into Consequencia values(5, 'pontos', 50, 'F', 'I', 50, 'Você se divertiu muito... Mas será que valeu a pena?')
+insert into Consequencia values(6, 'carreira', 50, 'I', 'F', 50, '')
+
+insert into Consequencia values(7, 'pontos', 50, 'F', 'I', 50, 'Você jogou video game a noite toda! Mas e os estudos?')
+insert into Consequencia values(8, 'pontos', 50, 'I', 'F', 50, 'Estudar é o que te dará futuro... Pena que não é o que te deixa feliz.')
+
+insert into Consequencia values(9, 'namoro', 1, 'M', '', 0, 'Agora você está namorando!!!')
+insert into Consequencia values(10, 'namoro', 0, 'T', '', 0, 'Você escolheu manter sua amizade como está, será que realmente voltará a ser como antes?')
+insert into Consequencia values(11, 'namoro', 1, 'F', '', 0, 'Agora você está namorando!!!')
+
+insert into Consequencia values(13, 'namoro', 1, '', '', 0, 'Ele(a) aceitou a nova chance! Tomara que as coisas melhorem...')
+insert into Consequencia values(14, 'namoro', 0, '', '', 0, 'Todo relacionamento chega a um fim! Foque em coisas boas para não perder a esperança!')
+
+insert into Consequencia values(15, 'pontos', 50, 'F', 'S', 50, 'A festa foi ótima pra sua felicidade! Agora para sua saúde é outra história...')
+insert into Consequencia values(16, 'pontos', 50, 'S', 'F', 50, 'Sua saúde agradece a pequena pausa, infelizmente você está arrependido porque teria se divertido muito.')
+
+insert into Consequencia values(17, 'pontos', 10, 'F', 'S', 50, 'Você escolheu o caminho da violência e acabou machucado... Entretanto até que te deixou um pouco feliz ter coragem de enfrentá-lo.')
+insert into Consequencia values(18, 'pontos', 10, 'F', 'S', 40, 'Não descer ao nível de seu amiguinho te fez se sentir superior, mas não o impediu de provocá-lo de novo')
+
+
+
 
