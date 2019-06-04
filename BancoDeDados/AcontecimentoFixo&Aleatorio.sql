9bbcb0b8c8d5837c09ba385912d54dac6de04801
FELIPE MELCHIOR DE BRITTO <u18200@academico.cotuca.unicamp.br>
2019-06-04 11:44:36 -0300

    consertando o banco de dados

--- BancoDeDados/AcontecimentoFixo&Aleatorio.sql
+++ BancoDeDados/AcontecimentoFixo&Aleatorio.sql
@@ -25,15 +25,22 @@ constraint fkEscolha2 foreign key(codEscolha) references Escolha(id)
 select * from AcontecimentoAleatorio
 drop table AcontecimentoAleatorio
 
--- 1 a 15
+-- até 15 anos	
 
-insert into AcontecimentoAleatorio values (1, 'Você tem uma festa no final de semana e uma prova segunda... Irá à festa ou estudará para a prova?', 1)
-insert into AcontecimentoAleatorio values (2, 'Jogar vídeo game ou estudar para a prova no dia seguinte?', 11)
-insert into AcontecimentoAleatorio values (3, 'Seu amiguinho te empurrou', 10)
+insert into AcontecimentoAleatorio values (1, 'Você tem uma festa no final de semana e uma prova segunda... Irá à festa ou estudará para a prova?', 20)
+insert into AcontecimentoAleatorio values (2, 'Jogar vídeo game ou estudar para a prova no dia seguinte?', 21)
+insert into AcontecimentoAleatorio values (3, 'Seu amigo te empurrou', 22)
+insert into AcontecimentoAleatorio values (4, 'Você estava jogando bola e perdeu ela no mato, vai procurá-la?Pode ser perigoso...'23)
+insert into AcontecimentoAleatorio values (5,)	
+insert into AcontecimentoAleatorio values (6,)
+insert into AcontecimentoAleatorio values (7,)
+insert into AcontecimentoAleatorio values (8,)
+
+
 
 insert into AcontecimentoAleatorio values (21, 'Seu melhor amigo tem sentimentos por você e te pediu em namoro', 5)
 insert into AcontecimentoAleatorio values (22, 'Sua melhor amiga tem sentimentos por você e te pediu em namoro', 6)
 insert into AcontecimentoAleatorio values (23, 'Seu namorado(a) terminou com você =(', 7)
 insert into AcontecimentoAleatorio values (24, 'Seu namorado(a) terminou com você =(', 8)
 insert into AcontecimentoAleatorio values (25, 'Você tem uma festa de aniversário e está muito doente, mas ainda assim deseja muito ir... O que fazer?', 9)
-insert into AcontecimentoAleatorio values (25, '', 11)
+insert into AcontecimentoAleatorio values (26, '', 11)
