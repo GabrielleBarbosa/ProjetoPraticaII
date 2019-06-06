89b3e620e1d213a0ad55fa5de0f603a6efb07fc9
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-06-06 11:59:38 -0300

    oie

---
+++ Site/YourLife/YourLife/Controllers/DireitosAutoraisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YourLife.Controllers
+{
+    public class DireitosAutoraisController : Controller
+    {
+        // GET: DireitosAutorais
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
