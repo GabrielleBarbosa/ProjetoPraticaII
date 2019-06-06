55706edf64e6839aa1e323753ef1f9237374031f
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-06-06 11:47:46 -0300

    tururu

--- Site/YourLife/YourLife/Controllers/UsuarioController.cs
+++ Site/YourLife/YourLife/Controllers/UsuarioController.cs
@@ -15,5 +15,29 @@ namespace YourLife.Controllers
             ViewBag.Usuario = Session["Usuario"];
             return View();
         }
+
+        public JsonResult ValidarLogin(string senha, string nick)
+        {
+            UsuarioDAO usuDAO = new UsuarioDAO();
+            Usuario usuarioExistente = usuDAO.BuscaPorNick(nick);
+            if (usuarioExistente != null && usuarioExistente.senha == senha)
+            {
+                Session["Usuario"] = usuarioExistente;
+                return Json(true);
+            }
+            return Json(false);
+        }
+
+        public ActionResult Alterar(Usuario usu)
+        {
+            UsuarioDAO daoU = new UsuarioDAO();
+            Usuario usuAntes = (Usuario)Session["Usuario"];
+
+            usuAntes.senha = usu.senha;
+            
+            daoU.Alterar(usuAntes);
+
+            return View("ConfiguracoesUsuario");
+        }
     }
 }
